629fba0f1e09f84bb7e917fceb684735a16c236d
HavallonTech <kestercode@gmail.com>
2024-01-29 00:33:53 +0100

    Fix my code

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -40,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -52,11 +52,9 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        elif self.__password is None:
+        if self.__password is None:
             return False
-        else:
-            # return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
-            return True
+        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
 
 if __name__ == '__main__':
@@ -86,10 +84,9 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    # if not user_1.is_valid_password(u_pwd):
     if not user_1.is_valid_password(u_pwd):
-        # print("is_valid_password should return True if it's the right \password")
-        print("True")
+        print("is_valid_password should return True if it's the right \
+password")
 
     if user_1.is_valid_password("Fakepwd"):
         print("is_valid_password should return False if it's not the right \
